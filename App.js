f108113515f7c2cf81ffeb0f68c4d595d8141cdf
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2024-10-16 12:00:05 +0700

    test

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeTabs from "./Screen/Home/Home_Screen"; // Import HomeTabs thay vì HomeScreen
+import { enableScreens } from "react-native-screens"; // Để tối ưu hóa màn hình
+import { AppRegistry } from "react-native";
+// Gọi enableScreens để kích hoạt tối ưu hóa
+enableScreens();
+
 import SignUp from "./Screen/Screen_SignUp/SignUp_Screen";
 import SignUp1 from "./Screen/Screen_SignUp/SignUp_01_Screen";
 import SignUp2 from "./Screen/Screen_SignUp/SignUp_02_Screen";
@@ -9,27 +13,20 @@ import SignUp3 from "./Screen/Screen_SignUp/SignUp_03_Screen";
 import SignUp4 from "./Screen/Screen_SignUp/SignUp_04_Screen";
 import Search from "./Screen/Search/Search_Screen";
 import SignIn from "./Screen/SignIn/SignIn_Screen";
+import HomeTabs from "./Screen/Home/Home_Screen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="HomeTabs">
         <Stack.Screen
-          name="Home"
-          component={HomeTabs}  // Dùng HomeTabs thay cho Home
+          name="HomeTabs"
+          component={HomeTabs}
           options={{
-            title: "Home",
-            headerShown: false, // Ẩn header
-          }}
-        />
-        <Stack.Screen
-          name="Search"
-          component={Search}
-          options={{
-            title: "Search",
-            headerShown: false, // Ẩn header
+            title: "HomeTabs",
+            headerShown: false,
           }}
         />
         <Stack.Screen
@@ -84,5 +81,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
+AppRegistry.registerComponent("main", () => App);
 export default App;
